1847511ddb069825ee2e76a6cddc728ee08b5335
Alessandro <a.sbarbati@miriodev.net>
2016-02-16 01:32:41 +0100

    mistake lib referal

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# AmazonStorePrice documentation build configuration file, created by
+# PsnStorePrice documentation build configuration file, created by
 # sphinx-quickstart on Sun Feb 14 00:04:51 2016.
 #
 # This file is execfile()d with the current directory set to its
@@ -48,7 +48,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'AmazonStorePrice'
+project = 'PsnStorePrice'
 copyright = '2016, Alessandro Sbarbati'
 author = 'Alessandro Sbarbati'
 
@@ -202,7 +202,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'AmazonStorePricedoc'
+htmlhelp_basename = 'PsnStorePricedoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -224,7 +224,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'AmazonStorePrice.tex', 'AmazonStorePrice Documentation',
+    (master_doc, 'PsnStorePrice.tex', 'PsnStorePrice Documentation',
      'Alessandro Sbarbati', 'manual'),
 ]
 
@@ -254,7 +254,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'amazonstoreprice', 'AmazonStorePrice Documentation',
+    (master_doc, 'psnstoreprice', 'PsnStorePrice Documentation',
      [author], 1)
 ]
 
@@ -268,8 +268,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'AmazonStorePrice', 'AmazonStorePrice Documentation',
-     author, 'AmazonStorePrice', 'One line description of project.',
+    (master_doc, 'PsnStorePrice', 'PsnStorePrice Documentation',
+     author, 'PsnStorePrice', 'One line description of project.',
      'Miscellaneous'),
 ]
 
